0969f9b4048d5cbdf0d058541cfb7a29c52b8b3d
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-01-02 17:02:25 -0500

    Added coditions only_if

--- recipes/firewall.rb
+++ recipes/firewall.rb
@@ -4,12 +4,15 @@ when 'oracle'
   when /^6/
     execute 'iptables' do
       command 'iptables -A INPUT -p udp -m udp --dport 1691 -j ACCEPT && service iptables save'
+      only_if { ::File.exist?('/sbin/iptables') }
       not_if "service iptables status|grep -i 'not running'"
     end
   when /^7/
     execute 'firewall' do
       command 'firewall-cmd --zone=public --permanent --add-port=1691/udp && systemctl restart firewalld.service'
+      only_if { ::File.exist?('/usr/bin/firewall-cmd') }
       not_if "firewall-cmd --state 2>&1|grep -i 'not running'"
+
     end
   end
 
