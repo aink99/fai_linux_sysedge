8198333f594d98ce50af8256cf736e19591a8549
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-01-03 16:31:30 -0500

    Changed the default source URL.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['fai_linux_sysedge']['src_url'] = 'https://glpi.fujitsucanada.net/downloads/Linux_x86_58.tar.gz'
+default['fai_linux_sysedge']['src_url'] = 'http://s1.fujitsucanada.net/downloads/Linux_x86_58.tar.gz'
 default['fai_linux_sysedge']['download_dir'] = '/tmp'
 default['fai_linux_sysedge']['target_dir'] = '/tmp/sysedge'
 default['fai_linux_sysedge']['snmp_read'] = 'C@unicenter3'
