6a6788ced050a820c04dd593a77cda4de9a93ddc
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-03-06 16:09:29 -0500

    Added logic to install even if the initd CA was present

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,12 +2,30 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
-# Check if installation is needed.
-unless ::File.exist?('/etc/init.d/CA-SystemEDGE')
+#Print attributes
+puts node['fai_linux_sysedge']['version']
+puts node['fai_linux_sysedge']['force']
+if node['fai_linux_sysedge']['force'] == 'true'
   include_recipe 'tar'
   if node['fai_linux_sysedge']['version'] == '58'
     include_recipe 'fai_linux_sysedge::sysedgev58'
     include_recipe 'fai_linux_sysedge::firewall'
+  end
+  if node['fai_linux_sysedge']['version'] == '59'
+    include_recipe 'fai_linux_sysedge::sysedgev59'
+    include_recipe 'fai_linux_sysedge::firewall'
+  end
+else  	
+  # Check if installation is needed.
+  unless ::File.exist?('/etc/init.d/CA-SystemEDGE')
+    include_recipe 'tar'
+    if node['fai_linux_sysedge']['version'] == '58'
+      include_recipe 'fai_linux_sysedge::sysedgev58'
+      include_recipe 'fai_linux_sysedge::firewall'
+    end
+    if node['fai_linux_sysedge']['version'] == '59'
+      include_recipe 'fai_linux_sysedge::sysedgev59'
+      include_recipe 'fai_linux_sysedge::firewall'
+    end
   end
 end
