c7ffb0fadb3e6fb90f209c07ce62cb901ddd4257
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-03-07 13:49:13 -0500

    changed url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['fai_linux_sysedge']['src_url'] = 'http://s1.fujitsucanada.net/downloads/Linux_x86_58.tar.gz'
+default['fai_linux_sysedge']['src_url'] = 'http://D0wml0ad3r3383:2121CrAzyAn1mAl78@s1.fujitsucanada.net/downloads/Linux_x86_58.tar.gz'
 default['fai_linux_sysedge']['download_dir'] = '/tmp'
 default['fai_linux_sysedge']['target_dir'] = '/tmp/sysedge'
 default['fai_linux_sysedge']['snmp_read'] = 'C@unicenter3'
